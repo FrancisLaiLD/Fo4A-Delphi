6e4f9984dc9d84530896b0d418d1d193c8439f60
FrancisLD <laidanghungptit@gmail.com>
2018-06-27 00:25:02 +0700

    [FrancisLai] Add Global varriables of Process name

--- DelphiXE8-Fo4Auto-Project/GlobalVars.pas
+++ DelphiXE8-Fo4Auto-Project/GlobalVars.pas
@@ -7,6 +7,9 @@ var
   GARENA_SIGNOUT_TITLE: string = 'Garena - Your Ultimate Game Platform';
   GARENA_SIGNIN_TITLE: string = 'Garena - Game Center';
 
+  GAME_PROCESS_NAME: string = '';
+  GARENA_PROCESS_NAME: string = '';
+
   // button click
     POSCLICK_SIGNIN_X: integer = 578;
     POSCLICK_SIGNIN_Y: integer = 434;
